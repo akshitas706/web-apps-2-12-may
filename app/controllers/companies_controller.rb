bb333a609914a0abc9039315a3682b0cb1500f71
Akshita Singh <akshita.singh1@kellogg.northwestern.edu>
2023-05-18 03:56:33 +0000

    2

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -16,16 +16,25 @@ class CompaniesController < ApplicationController
     # render companies/show view with details about Company
   end
 
-  # def new
+  def new
+    @company = Company.new 
   #   # render view with new Company form
-  # end
+  end
   
-  # def create
+  def create
+
   #   # start with a new Company
+@company= Company.new
+
   #   # assign user-entered form data to Company's columns
+  @company["name"]= params["company"]["name"]
+  @company["city"]= params["company"]["city"]
+  @company["state"]= params["company"]["state"]
   #   # save Company row
+@company.save
+
   #   # redirect user
-  # end
+  end
 
   # def edit
   #   # find a Company
